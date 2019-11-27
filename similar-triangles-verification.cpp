33cc6ea1d61c8ab50293ba9df0e228e53fd4ec14
xiafuning <xiafuning@gmail.com>
2019-11-27 22:25:02 +0100

    FIX: fix fake verification

--- similar-triangles-verification.cpp
+++ similar-triangles-verification.cpp
@@ -5,7 +5,8 @@ void printMemory(int memory[], int size)
 {
 	//std::cout << "print memory" << std::endl;
 	for (int i = 0; i < size; i++)
-		std::cout << memory[i] << std::endl;
+		std::cout << memory[i] << " ";
+	std::cout << std::endl;
 }
 
 void bubbleSort(int dataIndex, int memory[])
@@ -42,36 +43,51 @@ int main(int argc,char *argv[])
 	// memory and parameter initialisation
 	int memory[6] = {3, 3, 4, 4, 6, 8};
 	int index = 0;
-	//int dataCounter, flag;
+	int R_1 = 0;
+	int R_2 = 0;
 	int product_1 = 0;
 	int product_2 = 0;
+	
+	std::cout << "initial memory" << std::endl;
+	printMemory(memory, 6);
 
 	while (index < 2)
 	{
 		if (index == 1)
 		{
 			bubbleSort(3, memory);
+			std::cout << "memory after second sort" << std::endl;
 			printMemory(memory, 6);
 		}
 		else
 		{
-			bubbleSort(0, memory);
+			bubbleSort(0, memory);			
+			std::cout << "memory after first sort" << std::endl;
 			printMemory(memory, 6);
 		}
 		index++;
-	}
-
-	product_1 = memory[0] * memory[4];
-	product_2 = memory[1] * memory[3];
-
+	}	
+	R_1 = memory[0];
+	R_2 = memory[4];
+	product_1 = R_1 * R_2;
+	R_1 = memory[1];
+	R_2 = memory[3];
+	product_2 = R_1 * R_2;
 	if (product_1 == product_2)
-		std::cout << "these 2 triangles are similar!" << std::endl;
+	{
+		R_1 = memory[0];
+		R_2 = memory[5];
+		product_1 = R_1 * R_2;
+		R_1 = memory[2];
+		R_2 = memory[3];
+		product_2 = R_1 * R_2;
+		if (product_1 == product_2)
+			std::cout << "these 2 triangles are similar!" << std::endl;
+		else
+			std::cout << "these 2 triangles are not similar!" << std::endl;
+	}
 	else
 		std::cout << "these 2 triangles are not similar!" << std::endl;
 	
 	return 0;
 }
-
-
-
-
